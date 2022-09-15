636d1f0fad827f3e67bfd93de2aadb8c34163957
Ottorvord <113486493+Ottorvord@users.noreply.github.com>
2022-09-15 11:19:07 +0800

    Update mrc.py

--- mrc.py
+++ mrc.py
@@ -1,5 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
 url = "https://www.bing.com"
 
-from bs4 import BeautifulSoup
-soup = BeautifulSoup(url, 'html.parser')
-print(soup.prettify())
+req = requests.get(url)
+soup = BeautifulSoup(req.text, 'html.parser')
+
+blah = soup.find_all(text="Search")
+
+print(blah.prettify())
